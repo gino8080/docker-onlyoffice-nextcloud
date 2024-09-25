4784a2acfd60d1adfe76bce17da0d17a287fe58c
Magico <magie.cannizzaro@gmail.com>
2024-09-25 14:27:48 +0200

    ssl, preview, mysql db

--- set_configuration.sh
+++ set_configuration.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
-
 set -x
 
-docker exec -u www-data app-server php occ --no-warnings config:system:get trusted_domains >> trusted_domain.tmp
+# Funzione per aggiungere un dominio fidato
+add_trusted_domain() {
+    local domain=$1
+    TRUSTED_INDEX=$(docker exec -u www-data app-server php occ --no-warnings config:system:get trusted_domains | wc -l)
+    docker exec -u www-data app-server php occ --no-warnings config:system:set trusted_domains $TRUSTED_INDEX --value="$domain"
+}
 
-if ! grep -q "nginx-server" trusted_domain.tmp; then
-    TRUSTED_INDEX=$(cat trusted_domain.tmp | wc -l);
-    docker exec -u www-data app-server php occ --no-warnings config:system:set trusted_domains $TRUSTED_INDEX --value="nginx-server"
-fi
+# Aggiungi sempre i domini fidati
+add_trusted_domain "nginx-server"
+add_trusted_domain "onlyofficee.hopto.org"
 
-rm trusted_domain.tmp
-
+# Installa e configura OnlyOffice
 docker exec -u www-data app-server php occ --no-warnings app:install onlyoffice
-
 docker exec -u www-data app-server php occ --no-warnings config:system:set onlyoffice DocumentServerUrl --value="/ds-vpath/"
 docker exec -u www-data app-server php occ --no-warnings config:system:set onlyoffice DocumentServerInternalUrl --value="http://onlyoffice-document-server/"
 docker exec -u www-data app-server php occ --no-warnings config:system:set onlyoffice StorageUrl --value="http://nginx-server/"
 docker exec -u www-data app-server php occ --no-warnings config:system:set onlyoffice jwt_secret --value="secret"
+
+# Verifica i domini fidati
+echo "Trusted domains after configuration:"
+docker exec -u www-data app-server php occ --no-warnings config:system:get trusted_domains
